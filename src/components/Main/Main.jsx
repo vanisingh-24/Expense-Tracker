4c977f1e6dfebfdf6880ec0d3b6f710d5b2e29a9
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-24 18:59:44 +0530

    List component created

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider} from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
@@ -16,12 +18,12 @@ const Main = () => {
                    Try saying: Add income for $100 in Category Salary for Monday...
                </Typography>
                <Divider />
-               { /* Form */}
+               <Form />
            </CardContent>
            <CardContent className={classes.CardContent}>
                <Grid spacing={2} container>
                    <Grid item xs={12}>
-                       {/* List */}
+                       <List />
                    </Grid>
                </Grid>
            </CardContent>
