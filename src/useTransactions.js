25d1d14a183b5b4664446a6b188b2547533b1df7
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-28 18:53:37 +0530

    chart data

--- src/useTransactions.js
+++ src/useTransactions.js
@@ -19,5 +19,16 @@ const useTransactions = (title) => {
 
     const filteredCategories = categories.filter((c) => c.amount > 0);
 
-    
+    const chartData = {
+        datasets: [{
+            data: filteredCategories.map((c) => c.amount),
+            backgroundColor: filteredCategories.map((c) => c.color)
+        }],
+        labels: filteredCategories.map((c) => c.type)
+    }
+
+    return { total, chartData}
+
 }
+
+export default useTransactions;
