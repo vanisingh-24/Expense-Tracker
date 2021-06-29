920d09d9321eabcaa77553bad57bc7e2150002d1
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-06-29 22:01:26 +0530

    speechly configuration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
 
 import Main from './components/Main/Main';
 import Details from './components/Details/Details';
@@ -21,6 +22,11 @@ const App = () => {
                 <Details title="Expense" />
               </Grid>
            </Grid>
+
+           <PushToTalkButtonContainer>
+              <PushToTalkButton />
+              <ErrorPanel />
+           </PushToTalkButtonContainer>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-<Provider>
+    <SpeechProvider appId="a355910e-8a44-41cb-aaf5-689f0cb2db68" language="en-US">
+        <Provider>
 <App />
-</Provider>,
+</Provider>
+    </SpeechProvider>,
 document.getElementById('root'));
