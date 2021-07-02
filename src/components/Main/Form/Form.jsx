3d1e75cd8250651eee9356df71c780f2f87a8e94
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-07-02 20:04:03 +0530

    snackbar component

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -20,12 +20,14 @@ const Form = () => {
     const { formData, setFormData } = useState(initialState);
     const { addTransaction } = useContext(ExpenseTrackerContext);
     const { segment } = useSpeechContext();
+    const [open, setOpen] = useState(false);
 
 const createTransaction = () => {
     if(Number.isNaN(Number(formData.amount)) || !formData.date.includes('-')) return;
     
     const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
 
+    setOpen(true);
     addTransaction(transaction);
     setFormData(initialState);
 }
@@ -74,6 +76,7 @@ const selectedCategories = formData.type === 'Income' ? incomeCategories : expen
 
     return (
         <Grid container spacing={2}>
+            <CustomizedSnackbar open={open} setOpen={setOpen} />
             <Grid item xs={12}>
                 <Typography align="center" variant="subtitle2" gutterBottom>
                     {segment ? (
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,17 +8,18 @@ import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
+    const { balance } = useContext(ExpenseTrackerContext);
 
     return (
         <Card className={classes.root} >
            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
            <CardContent >
-               <Typography align="center" variant="h5">Total Balance $100</Typography>
+    <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                <Typography variant="subtitle1" style={{lineHeight: '1.5em', marginTop: '20px' }}>
                    {/* InfoCard... */}
                    Try saying: Add income for $100 in Category Salary for Monday...
                </Typography>
-               <Divider />
+               <Divider /> 
                <Form />
            </CardContent>
            <CardContent className={classes.CardContent}>
