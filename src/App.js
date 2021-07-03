49e77ca75be5c85d1efb87ae95a9012a5485d27f
vanisingh-24 <61752418+vanisingh-24@users.noreply.github.com>
2021-07-03 22:59:56 +0530

    code clean up

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel } from '@speechly/react-ui';
+import React from "react";
+import { Grid } from "@material-ui/core";
+import {
+  PushToTalkButton,
+  PushToTalkButtonContainer,
+  ErrorPanel,
+} from "@speechly/react-ui";
 
-import Main from './components/Main/Main';
-import Details from './components/Details/Details';
-import useStyles from './styles';
+import Main from "./components/Main/Main";
+import Details from "./components/Details/Details";
+import useStyles from "./styles";
 
 const App = () => {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div>
-           <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{height: '100vh'}}>
-              <Grid item xs={12} sm={4}>
-                <Details title="Income" />
-              </Grid>
-              <Grid item xs={12} sm={3}>
-                <Main />
-              </Grid>
-              <Grid item xs={12} sm={4}>
-                <Details title="Expense" />
-              </Grid>
-           </Grid>
+  return (
+    <div>
+      <Grid
+        className={classes.grid}
+        container
+        spacing={0}
+        alignItems="center"
+        justify="center"
+        style={{ height: "100vh" }}
+      >
+        <Grid item xs={12} sm={4}>
+          <Details title="Income" />
+        </Grid>
+        <Grid item xs={12} sm={3}>
+          <Main />
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Details title="Expense" />
+        </Grid>
+      </Grid>
 
-           <PushToTalkButtonContainer>
-              <PushToTalkButton />
-              <ErrorPanel />
-           </PushToTalkButtonContainer>
-        </div>
-    )
-}
+      <PushToTalkButtonContainer>
+        <PushToTalkButton />
+        <ErrorPanel />
+      </PushToTalkButtonContainer>
+    </div>
+  );
+};
 
 export default App;
--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,18 +1,18 @@
 const formatDate = (date) => {
-    const d = new Date(date);
-    let month = `${d.getMonth() + 1}`;
-    const day = `${d.getDate()}`;
-    const year = d.getFullYear();
+  const d = new Date(date);
+  let month = `${d.getMonth() + 1}`;
+  const day = `${d.getDate()}`;
+  const year = d.getFullYear();
 
-    if(month.length < 2) {
-        month = `0${month}`
-    }
+  if (month.length < 2) {
+    month = `0${month}`;
+  }
 
-    if(day.length < 2) {
-        day = `0${day}`
-    }
+  if (day.length < 2) {
+    day = `0${day}`;
+  }
 
-    return [year, month, day].join('-');
-}
+  return [year, month, day].join("-");
+};
 
 export default formatDate;
