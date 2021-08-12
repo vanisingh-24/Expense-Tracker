c3c4c45f7cf157ed07864a2d0568fa7471d0715b
Vani Singh <vanisingh6612@gmail.com>
2021-08-12 23:07:02 +0530

    component file changed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,13 @@
-import React, { useContext } from "react";
-import {
-  Card,
-  CardHeader,
-  CardContent,
-  Typography,
-  Grid,
-  Divider,
-} from "@material-ui/core";
-import { ExpenseTrackerContext } from "../../context/context";
-
-import useStyles from "./styles";
-import Form from "./Form/Form";
-import List from "./List/List";
+import React, { useState, useEffect, useContext } from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+import { useSpeechContext } from '@speechly/react-client';
+import { ExpenseTrackerContext } from '../../context/context';
+import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
 import InfoCard from '../InfoCard';
 
-const Main = () => {
+const ExpenseTracker = () => {
   const classes = useStyles();
   const { balance } = useContext(ExpenseTrackerContext);
 
@@ -22,21 +15,15 @@ const Main = () => {
     <Card className={classes.root}>
       <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
       <CardContent>
-        <Typography align="center" variant="h5">
-          Total Balance ${balance}
-        </Typography>
-        <Typography
-          variant="subtitle1"
-          style={{ lineHeight: "1.5em", marginTop: "20px" }}
-        >
-          {/* InfoCard... */}
+        <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
           <InfoCard />
         </Typography>
         <Divider className={classes.divider} />
         <Form />
       </CardContent>
-      <CardContent className={classes.CardContent}>
-        <Grid spacing={2} container>
+      <CardContent className={classes.cartContent}>
+        <Grid container spacing={2}>
           <Grid item xs={12}>
             <List />
           </Grid>
@@ -46,4 +33,4 @@ const Main = () => {
   );
 };
 
-export default Main;
+export default ExpenseTracker;
